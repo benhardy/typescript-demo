a2b8060fdacc38ab1d978b6572bd83cb11686bed
Ben Hardy <benhardy+github@gmail.com>
2017-07-20 07:47:53 -0400

    initial commit

---
+++ hello.js
@@ -0,0 +1,24 @@
+var express = require('express');
+ 
+var app = express.createServer(express.logger());
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+ 
+app.put('/', function(req, res) {
+  // unless the right content-type header is supplied, body will be undefined
+  // e.g.: curl -X PUT -d '{"dog":"mort"}' -H 'Content-Type: application/json' http://localhost:3000/
+  var body = req.body;
+  var dog = body.dog; 
+   
+  console.log("body is : "  + JSON.stringify(body));
+  console.log("dog is : "  + dog);
+       
+  res.contentType('json');
+  res.send(JSON.stringify({ status: "success" }));
+});
+ 
+var port = process.env.PORT || 3000;
+ 
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
