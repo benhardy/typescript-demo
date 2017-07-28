548f6cecf8263a532ba02625ebab56b8b0d44d54
Ben Hardy <benhardy+github@gmail.com>
2017-07-27 22:24:58 -0400

    a little refactoring of scrolly js

--- src/public/js/main.ts
+++ src/public/js/main.ts
@@ -1,26 +1,28 @@
-function actualSize(elementId:string, styleProperty:string):number {
+function elementDimensionById(elementId:string, styleProperty:string):number {
     let el = document.getElementById(elementId);
-    let prop = window.getComputedStyle(el).getPropertyValue(styleProperty);
+    return elementDimension(el, styleProperty);
+}
+
+function elementDimension(element:HTMLElement, styleProperty:string):number {
+    let prop = window.getComputedStyle(element).getPropertyValue(styleProperty);
     return Number(prop.replace(/[^0-9.]+/, ''));
 }
 
 window.onload = () => {
     let body = document.getElementById("body");
-    let heading = document.getElementById("big_heading");
-    let header = document.getElementById("headerbar");
+    let headerHeading = document.getElementById("big_heading");
+    let headerBox = document.getElementById("headerbar");
 
-    let initFontHeight = actualSize("big_heading", "font-size");
-    let initHeadHeight = actualSize("headerbar", "height");
-    let ratio = initFontHeight / initHeadHeight;
-    let minHeaderHeight = 32;
+    let initHeadingFontSize = elementDimension(headerHeading, "font-size");
+    let initHeaderBoxHeight = elementDimension(headerBox, "height");
+    let headingToHeaderRatio = initHeadingFontSize / initHeaderBoxHeight;
+    let minHeaderHeight = initHeaderBoxHeight / 5;
 
     body.onscroll = () => {
         let top = body.scrollTop;
-        let newHeadHeight = initHeadHeight - top;
-        if (newHeadHeight < minHeaderHeight) { newHeadHeight = minHeaderHeight; }
-
-        header.style.height = newHeadHeight + 'px';
-        header.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)"
-        heading.style.fontSize = (newHeadHeight * ratio) + 'px';
+        let newHeadHeight = Math.max(initHeaderBoxHeight - top, minHeaderHeight);
+        headerBox.style.height = newHeadHeight + 'px';
+        headerBox.style.boxShadow = "0px 0px 10px rgba(0,0,0,0.5)"
+        headerHeading.style.fontSize = (newHeadHeight * headingToHeaderRatio) + 'px';
     };
 };
